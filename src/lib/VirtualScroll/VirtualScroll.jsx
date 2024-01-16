18e0b18762bbeb88cd70c2f9d6966471acdb31a6
김민재 <kimminje7267@naver.com>
2024-01-16 12:29:39 +0900

    feat: VirtualScroll add style props

--- src/lib/VirtualScroll/VirtualScroll.jsx
+++ src/lib/VirtualScroll/VirtualScroll.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import { useVirtualScroll } from "./VirtualScroll.hook";
 import "./VirtualScroll.styles.css";
 
-export const VirtualScroll = ({ list, onItemSelected, width }) => {
+export const VirtualScroll = ({
+  list,
+  onItemSelected,
+  width,
+  listStyle,
+  centerItemStyle,
+  itemStyle,
+}) => {
   const {
     state: { scrollList, scrollRef, centerIndex },
   } = useVirtualScroll({
@@ -14,11 +21,15 @@ export const VirtualScroll = ({ list, onItemSelected, width }) => {
     <div ref={scrollRef} width={width} className="Container">
       <div className="List">
         {scrollList.map((item, index) => (
-          <div key={index}>
+          <div key={index} style={listStyle}>
             {index === centerIndex ? (
-              <div className="CenterItem">{item}</div>
+              <div className="CenterItem" style={centerItemStyle}>
+                {item}
+              </div>
             ) : (
-              <div className="Item">{item}</div>
+              <div className="Item" style={itemStyle}>
+                {item}
+              </div>
             )}
           </div>
         ))}
